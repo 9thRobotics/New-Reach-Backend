3ac1afd1814bf5f93f35d6c07acfb41c3401a701
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-18 23:57:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send('Server is running!');
 });
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 app.post('/buyTokens', async (req, res) => {
     const { recipient, amount } = req.body;
