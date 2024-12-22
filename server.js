c9755a46b963fad9004a3c110754f91fa48254e0
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-22 13:51:51 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const purchaseRoute = require('./routes/purchase');
 
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://9throbotics.github.io/Reach-Frontend/', // Replace with your frontend's URL
-  methods: 'GET,POST', // Specify
+// Initialize Express app
+const app = express();
 
+// Configure environment variables
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Enable CORS for specified origin
+app.use(cors({
+  origin: 'https://9throbotics.github.io/Reach-Frontend/', // Replace with your frontend's URL
+  methods: ['GET', 'POST'], // Specify allowed methods
+}));
 
+// Middleware to parse JSON requests
 app.use(express.json());
 
 // Routes
 app.use('/api/purchase', purchaseRoute);
 
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Backend server running on http://localhost:${PORT}`);
+  console.log(`Backend server running on http://localhost:${PORT}`);
 });
