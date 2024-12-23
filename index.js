ba778be8480609645d6f970028f5a6f44ee5f4aa
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-23 12:40:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config(); // Load environment variables from .env
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-console.log(`Server is starting...`);
-
-// Debug: Log environment variables
-console.log('Environment Variables:', {
-  PRIVACY_KEY: process.env.PRIVACY_KEY,
-  WALLET_ADDRESS: process.env.WALLET_ADDRESS,
-  PORT: process.env.PORT || 5000,
+app.get('/test', (req, res) => {
+  res.send('Test route is working!');
 });
 
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-});
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
