bcbe1423cb285b12f3568849cb6015e8c2ea48fc
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-24 14:53:22 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.post('/api/users', (req, res) => {
     console.log('New User:', user);
     res.status(201).send(user);
 });
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/reach-backend', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
