8e19bc323b3ce178e95ceea3e5182a18c1feea64
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-24 14:43:04 -0800

    Create testserver.js

---
+++ testserver.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Test server is running!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Test server is running on http://localhost:${PORT}`);
+});
