7956eedadde78f539bf60852dd06c9d1d0f70ceb
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-23 17:10:10 -0800

    Update server.js

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ app.use(express.json());
 app.use('/api/purchase', purchaseRoute);
 
 // Start the server
-const API_BASE_URL = " https://reach-token-heroku-app-f5aa74057eec.herokuapp.com/
-";
+const API_BASE_URL = " https://reach-token-heroku-app-f5aa74057eec.herokuapp.com/";
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
