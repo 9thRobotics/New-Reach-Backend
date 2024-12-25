0eb6d29b7769fe63e1542a277aec433531b15427
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-24 18:13:51 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const bcrypt = require('bcrypt');
 const app = express();
 app.use(express.json());
 
-// Database connection
-mongoose.connect('mongodb://localhost:27017/reach-backend', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect('mongodb://localhost:27017/reach-backend')
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Authentication route
