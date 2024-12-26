f714204ee84ff590543b3dd2a3a13db1cb108c76
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-26 13:30:55 -0800

    Update testserver.js

--- testserver.js
+++ testserver.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send('Test server is running!');
 });
 
-const PORT = 3000;
-
 app.listen(PORT, () => {
     console.log(`Test server is running on http://localhost:${PORT}`);
+});
+
+app.on('error', (err) => {
+    console.error('Server error:', err);
 });
