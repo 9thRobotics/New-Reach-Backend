d67430fd51ba30efb3e32025e42bad6777db058f
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-25 22:58:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
