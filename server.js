dcfde5fb86467c920567c92cc1525332de1c14fc
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-25 23:03:53 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (err) => {
+  console.error('Failed to start server:', err);
+});
