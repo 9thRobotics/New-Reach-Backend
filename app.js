3b68aa65481c98968611197f9ff7a66496837fd5
9thRobotics <phillipdfilkins@hotmail.com>
2024-12-31 12:47:27 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+// Middleware to force HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
+// Define your routes here
+app.get('/', (req, res) => {
+  res.send('Hello, secure world!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+  }
+  next();
+});
+
