5413be11181aed6329c43d9b62aa0e55a49751da
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-03 15:36:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Middleware to force HTTPS in production
 app.use((req, res, next) => {
@@ -18,6 +22,12 @@ app.use('/api/tokens', tokensRouter);
 // Define your routes here
 app.get('/', (req, res) => {
   res.send('Hello, secure world!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // Start the server
