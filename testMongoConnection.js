6a6c99056a744f4656cc111ce6dbeec65e03ac91
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-04 22:21:02 -0800

    Update testMongoConnection.js

--- testMongoConnection.js
+++ testMongoConnection.js
@@ -1,16 +1,7 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI || 'MONGO_URI=mongodb+srv://phillip:securePass123@cluster0.mongodb.net/reachDB?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://phillipdfilkins:securePass123@cluster0.yt6an.mongodb.net/reach_database?retryWrites=true&w=majority';
 
-async function testConnection() {
-    try {
-        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('Successfully connected to MongoDB!');
-        mongoose.connection.close();
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-    }
-}
-
-testConnection();
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch(err => console.error('Connection error:', err));
