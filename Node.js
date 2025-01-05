9eafb7a52e1bef2cb2279c1b17b21f349b91447c
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-05 11:12:37 -0800

    Update Node.js

---
+++ Node.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// Replace `process.env.MONGO_URI` with your connection string if you're not using environment variables
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {})
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
