0b72339eaa5acb7193f6faa93c6da9c135ceb51f
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-04 19:57:48 -0800

    Update testMongoConnection.js

--- testMongoConnection.js
+++ testMongoConnection.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
