7a82a6951ca3c5c65c7e2b9314687e330ffc8479
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-09 12:35:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const infuraUrl = "https://sepolia.infura.io/v3/e402796c27ef40fcae5f8d4ef688599f";
-
-// Your server setup and other code here
-
-console.log(`Connected to Infura at ${infuraUrl}`);
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
-const infuraUrl = "https://sepolia.infura.io/v3/e402796c27ef40fcae5f8d4ef688599f";
+const infuraUrl = process.env.INFURA_URL;
 
 app.get('/', (req, res) => {
   res.send(`Connected to Infura at ${infuraUrl}`);
