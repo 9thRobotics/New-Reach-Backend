26dc91ab8f4dca0337f714258e872fb7c3113e8e
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-09 12:22:21 -0800

    Update server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const infuraUrl = "https://sepolia.infura.io/ v3/e402796c27ef40fcae5f8d4ef688599f ";
+
+// Your server setup and other code here
+
+console.log(`Connected to Infura at ${ v3/e402796c27ef40fcae5f8d4ef688599f }`);
