faa52dac4bf939ff43f869f6d57086896ac4f357
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-10 20:09:52 -0800

    Update Sale.js

--- model/Sale.js
+++ model/Sale.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const SaleSchema = new mongoose.Schema({
+  walletAddress: String,
+  amount: Number,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Sale = mongoose.model('Sale', SaleSchema);
+
+module.exports = Sale;
