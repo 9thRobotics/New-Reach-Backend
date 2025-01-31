e06db43b1b60cd360ef1de271f9dde84fd515d65
9thRobotics <phillipdfilkins@hotmail.com>
2025-01-30 16:20:51 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
-
 module.exports = {
-  solidity: "0.8.28",
-  networks: {
-    goerli: {
-      url: process.env.GOERLI_RPC_URL || '',
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    solidity: {
+        version: "0.8.28",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || '',
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL || '',
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+        },
     },
-    localhost: {
-      url: "http://127.0.0.1:8545",
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
     },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
 };
