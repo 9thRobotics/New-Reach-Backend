ce78dc7dc026a58721ca0a387ecf4d263ac53f26
9thRobotics <phillipdfilkins@hotmail.com>
2025-02-01 17:57:02 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,12 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || "",
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   paths: {
     artifacts: "./artifacts",
