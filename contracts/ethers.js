94ddf38475dca18b355f4c2c4c1acfe6eb5a07ed
9thRobotics <phillipdfilkins@hotmail.com>
2025-02-01 19:57:49 -0800

    Update ethers.js

--- contracts/ethers.js
+++ contracts/ethers.js
@@ -1,1 +1,12 @@
+const { ethers } = require("ethers");
 
+const abi = [
+  // Add the ABI of your contract constructor here
+];
+const iface = new ethers.utils.Interface(abi);
+
+const encodedArgs = iface.encodeDeploy([
+  // Add your constructor arguments here
+]);
+
+console.log(encodedArgs);
