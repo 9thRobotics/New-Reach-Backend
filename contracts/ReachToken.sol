fc5715854013d89319c7dd7f5c4cbf84802c3752
9thRobotics <phillipdfilkins@hotmail.com>
2025-02-15 13:35:22 -0800

    Update ReachToken.sol

--- contracts/ReachToken.sol
+++ contracts/ReachToken.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
