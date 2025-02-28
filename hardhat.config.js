57c1aa0274ab6b9b1291ccf56f745d6b6979dd55
9thRobotics <phillipdfilkins@hotmail.com>
2025-02-27 18:31:55 -0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
-require("@nomiclabs/hardhat-etherscan"); // Ensure Etherscan plugin is enabled
-require("@nomiclabs/hardhat-ethers");
-require("@openzeppelin/hardhat-upgrades"); // Important for upgradeable contracts
 
 module.exports = {
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+  solidity: "0.8.20",
+  networks: {
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: ["YOUR_WALLET_PRIVATE_KEY"],
     },
+  },
+  etherscan: {
+    apiKey: "YOUR_ETHERSCAN_API_KEY",
+  },
+};
   },
   networks: {
     sepolia: {
