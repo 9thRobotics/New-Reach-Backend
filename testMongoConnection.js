00a440fa4584f3f0e7b4b13ccbe808600bad0b99
9th Robotics <phillipdfilkins@hotmail.com>
2025-03-05 16:16:22 -0800

    Update testMongoConnection.js

--- testMongoConnection.js
+++ testMongoConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = "mongodb+srv://phillipdfilkins:5ns7Vy51BbfMcgJY@cluster0.yt6an.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Replace <username> and <password> with your actual credentials
 mongoose.connect(mongoURI, {})
