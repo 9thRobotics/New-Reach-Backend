a2ebe3fb4c06dad6c55ae98fdeb09770f7ca796c
9th Robotics <phillipdfilkins@hotmail.com>
2025-03-09 10:42:29 -0700

    Update testMongoConnection.js

--- testMongoConnection.js
+++ testMongoConnection.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Remove this line
+
+const connectDB = require('../../config/db'); // Add this line
+connectDB(); // Call this at the start of your file
 
 // Replace with your actual MongoDB connection string
 const mongoURI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
