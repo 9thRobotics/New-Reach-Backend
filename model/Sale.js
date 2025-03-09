5b5f87f8cf468760b19d37ce121210ceacb5ccb3
9th Robotics <phillipdfilkins@hotmail.com>
2025-03-09 10:41:45 -0700

    Update Sale.js

--- model/Sale.js
+++ model/Sale.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Remove this line
+
+const connectDB = require('../../config/db'); // Add this line
+connectDB(); // Call this at the start of your file
 
 const SaleSchema = new mongoose.Schema({
   walletAddress: String,
