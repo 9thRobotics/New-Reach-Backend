be34cd5ad0ff3a37498ec1e8adcc5d90764831de
9th Robotics <phillipdfilkins@hotmail.com>
2025-03-26 20:13:31 -0700

    Update Token.js

--- models/Token.js
+++ models/Token.js
@@ -1,11 +1,12 @@
+// models/Token.js
 const mongoose = require('mongoose');
 
 const TokenSchema = new mongoose.Schema({
-  availableTokens: {
-    type: Number,
-    required: true,
-    default: 1000000, // Initial amount of tokens
-  },
+  name: { type: String, required: true },
+  symbol: { type: String, required: true },
+  address: { type: String, required: true, unique: true },
+  totalSupply: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Token', TokenSchema);
